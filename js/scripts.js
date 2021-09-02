be9f2539cbee499fa1bbf27e5c87e7b57b710842
A Muse <museabdirahman@gmail.com>
2021-09-03 00:26:51 +0100

    added form submit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -38,3 +38,26 @@ if (scrollTop) {
     window.scrollTo(0, 0);
   });
 }
+
+let form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  let status = document.getElementById("my-form-status");
+  let data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Thank you for your submission!";
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+    });
+}
+form.addEventListener("submit", handleSubmit);
